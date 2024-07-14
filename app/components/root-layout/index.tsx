2e1cea4bb8f1fc37a4e8d71214c7887e75cf1ece
summerscar <shen.yus@163.com>
2024-07-14 22:57:35 +0900

    ✨ feat: add SpeedInsights

--- app/components/root-layout/index.tsx
+++ app/components/root-layout/index.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import "github-markdown-css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export async function DefaultLayout({
 	isAdmin = false,
@@ -32,6 +33,7 @@ export async function DefaultLayout({
 						{children}
 					</main>
 				</NextIntlClientProvider>
+				<SpeedInsights />
 			</body>
 		</html>
 	);

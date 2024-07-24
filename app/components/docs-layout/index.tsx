327e41e97b35d2f43d960e34965d0b4ff10ad44d
summerscar <shen.yus@163.com>
2024-07-25 00:15:45 +0900

    🦄 refactor: opt html tag

--- app/components/docs-layout/index.tsx
+++ app/components/docs-layout/index.tsx
@@ -7,13 +7,12 @@ function DocsLayout({
 }: PropsWithChildren<{ category: ReactNode }>) {
 	return (
 		<div className="flex w-full">
-			<div className="self-start w-64 flex-none sticky top-[--header-height] max-h-[calc(100vh-var(--header-height))] overflow-auto">
-				{/* // TODO: Parallel Routes */}
+			<nav className="self-start w-64 flex-none sticky top-[--header-height] max-h-[calc(100vh-var(--header-height))] overflow-auto">
 				{category}
-			</div>
-			<div className="markdown-body flex-auto p-8 border-l-2 border-slate-900/10">
+			</nav>
+			<section className="flex-auto p-8 border-l-2 border-slate-900/10">
 				{children}
-			</div>
+			</section>
 		</div>
 	);
 }

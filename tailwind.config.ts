c8ddc53867509d057fd7fd637607be78d302ac03
summerscar <shen.yus@163.com>
2024-07-26 23:57:48 +0900

    ✨ feat: opt dark theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 const config: Config = {
+	darkMode: ["selector", '[data-theme="dark"]'],
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",

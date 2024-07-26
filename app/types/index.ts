0db4de589b645d5296fa28b9f585f701ff909356
summerscar <shen.yus@163.com>
2024-07-26 23:57:48 +0900

    ✨ feat: opt dark theme

--- app/types/index.ts
+++ app/types/index.ts
@@ -5,6 +5,13 @@ export enum Levels {
 	Intermediate = "intermediate",
 }
 
+export const THEME_DARK_KEY = "theme-dark";
+
+export enum Themes {
+	Light = "nord",
+	Dark = "dark",
+}
+
 export type LevelParams = {
 	level: Levels;
 };

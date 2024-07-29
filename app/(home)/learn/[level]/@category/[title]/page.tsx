7cc6866adb56961aae568e6580a3974cef91dcb0
summerscar <shen.yus@163.com>
2024-07-29 23:50:43 +0900

    🐞 fix: ts build error

--- app/(home)/learn/[level]/@category/[title]/page.tsx
+++ app/(home)/learn/[level]/@category/[title]/page.tsx
@@ -1,8 +1,5 @@
 import { DocsCategory } from "@/components/docs-category";
 import type { DocsTitleParams, LevelParams } from "@/types";
-import { generateMetadata } from "../../layout";
-
-export { generateMetadata };
 
 const Category = ({ params }: { params: LevelParams & DocsTitleParams }) => {
 	return <DocsCategory level={params.level} title={params.title} />;
--- app/(home)/learn/[level]/page.tsx
+++ app/(home)/learn/[level]/page.tsx
@@ -1,6 +1,23 @@
-import { type LevelParams, Levels } from "@/types";
+import { type DocsTitleParams, type LevelParams, Levels } from "@/types";
+import { getServerI18n } from "@/utils/i18n";
+import type { Metadata } from "next";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+
+// 这层 layout 获取不到下层 title 的动态参数，下层页面直接导入使用
+export async function generateMetadata({
+	params,
+}: {
+	params: LevelParams & Partial<DocsTitleParams>;
+}): Promise<Metadata> {
+	const t = await getServerI18n("Header");
+	const tIndex = await getServerI18n("Index");
+	const level = params.level;
+	const subTitle = params.title ? params.title : t(level);
+	return {
+		title: `${tIndex("title")}-${subTitle}`,
+	};
+}
 
 // TODO: mdx
 // https://www.zenryoku-kun.com/new-post/nextjs-mdx-remote

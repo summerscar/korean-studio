d2c0f99e2fe9d707cd7b1069deb93788e9ce40bc
summerscar <shen.yus@163.com>
2024-08-17 19:46:47 +0900

    ✨ feat(meta): add icon and og image

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,12 +1,37 @@
 import { getServerI18n } from "@/utils/i18n";
 import { auth } from "auth";
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = async () => {
 	const session = await auth();
 	const t = await getServerI18n("Header");
-	console.log("[Header]", session);
+
+	/*
+	TODO: active
+	https://github.com/vercel/next.js/issues/43704#issuecomment-2090798307
+	const path = getPathname().path;
+	console.log("[path]", path);
+	*/
+	const path = "";
+	const headerConfig = [
+		{
+			href: "/learn/beginner",
+			label: `🚧${t("beginner")}`,
+			active: path.includes("/learn/beginner"),
+		},
+		{
+			href: "/learn/intermediate",
+			label: `🚧${t("intermediate")}`,
+			active: path.includes("/learn/intermediate"),
+		},
+		{
+			href: "/topik",
+			label: "🚧TOPIK",
+			active: path.includes("/topik"),
+		},
+	];
 	return (
 		<header className="sticky top-0 h-[--header-height] flex border-b border-slate-900/10 w-full backdrop-blur-lg select-none bg-slate-300/10 text-base-content">
 			<div className="w-full px-4 flex justify-between items-center">
@@ -21,15 +46,15 @@ const Header = async () => {
 					/>
 				</Link>
 				<div className="flex">
-					<Link href="/learn/beginner" className="mr-4">
-						<span>🚧{t("beginner")}</span>
-					</Link>
-					<Link href="/learn/intermediate" className="mr-4">
-						<span>🚧{t("intermediate")}</span>
-					</Link>
-					<Link href="/topik" className="mr-4">
-						<span>🚧TOPIK</span>
-					</Link>
+					{headerConfig.map(({ href, label }) => (
+						<Link
+							key={href}
+							href={href}
+							className={clsx("mr-4 hover:underline")}
+						>
+							<span>{label}</span>
+						</Link>
+					))}
 					<span>
 						{session ? (
 							<div>

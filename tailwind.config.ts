044eb7ef994ee84cd98656c765cf31c4d938eb77
summerscar <shen.yus@163.com>
2024-08-20 21:21:25 +0900

    🐞 fix(tailwind): dynamic create element

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,5 +44,7 @@ const config: Config = {
 		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
 		themeRoot: ":root", // The element that receives theme color CSS variables
 	},
+	// https://medium.com/@achronus/solving-a-niche-frontend-problem-dynamic-tailwind-css-classes-in-react-da5f513ecf6a
+	safelist: ["alert-error", "alert-success", "alert-warning", "alert-info"],
 };
 export default config;

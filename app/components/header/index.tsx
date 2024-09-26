838b1f881cf3c082376653053455f82ac2ae05f3
summerscar <shen.yus@163.com>
2024-09-26 19:54:02 +0900

    🌈 style(header): active style

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -30,7 +30,7 @@ const Header = async () => {
 				<Link href="/">
 					<HomeIcon width={32} height={32} viewBox="0 0 24 24" />
 				</Link>
-				<div className="flex">
+				<div className="flex items-center">
 					{<ActiveLinks links={headerConfig(t)} />}
 					<span>
 						{session ? (

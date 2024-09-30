f6c2b9cedb146184b17a786d4b1cdb278ee52267
summerscar <shen.yus@163.com>
2024-09-30 20:29:10 +0900

    🦄 refactor(topik): test page

--- app/(home)/topik/[level]/[no]/page.tsx
+++ app/(home)/topik/[level]/[no]/page.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { ActionBar } from "./_components/action-bar";
 import { AnswerPanel } from "./_components/answer-panel";
-import { TestCutDown } from "./_components/count-down";
+import { QuestionCard } from "./_components/question-card";
 import type { TopikLevelType } from ".keystone/types";
 
 export async function generateMetadata({
@@ -34,68 +34,22 @@ export default async function NoPage({
 		return <div>Questions not found</div>;
 	}
 
-	const topikQuestions = topikListByLevelAndNo as TopikQuestion[];
+	const topikQuestions = JSON.parse(
+		JSON.stringify(topikListByLevelAndNo),
+	) as TopikQuestion[];
+
 	return (
 		<div>
 			<h1 className="text-2xl font-bold">
 				{topikQuestions[0].year}년 제{topikQuestions[0].no}회
-				{isTesting && (
-					<>
-						{" "}
-						<TestCutDown timeLeft={timeLeft} />
-					</>
-				)}
 			</h1>
-			<div className="flex items-start">
-				<div className="flex-auto">
-					{topikQuestions.map((topikQuestion) => (
-						<div
-							className="flex flex-col my-4"
-							id={topikQuestion.questionNumber.toString()}
-							key={topikQuestion.id}
-						>
-							<h1>{topikQuestion.questionStem}</h1>
-							<div>
-								<Link
-									className="hover:underline font-bold"
-									href={`/topik/${level}/${no}/${topikQuestion.questionNumber}`}
-								>
-									{topikQuestion.questionNumber}.
-								</Link>
-								<span> </span>
-								{topikQuestion.questionContent}
-							</div>
-							<h3>
-								{topikQuestion.options.map((option, index) => {
-									const id =
-										topikQuestion.questionNumber.toString() + index.toString();
-									return (
-										<div key={option.content}>
-											{/* TODO: group radio */}
-											<input
-												type="radio"
-												className="radio radio-xs radio-secondary"
-												disabled={!isTesting}
-												name="options"
-												value={index}
-												id={id}
-											/>
-											<span> </span>
-											<label htmlFor={id}>
-												{index + 1}. {option.content}
-											</label>
-										</div>
-									);
-								})}
-							</h3>
-							<h4>{topikQuestion.explanation}</h4>
-						</div>
-					))}
-				</div>
-				<AnswerPanel topikQuestions={topikQuestions} />
-			</div>
-
-			<ActionBar level={level} no={no} isTesting={isTesting} />
+			<QuestionCard
+				topikQuestions={topikQuestions}
+				level={level}
+				no={no}
+				timeLeft={timeLeft}
+				isTesting={isTesting}
+			/>
 		</div>
 	);
 }

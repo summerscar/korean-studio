6bb83d33f24f9a89ed122ca7a9e24c785bed7959
summerscar <shen.yus@163.com>
2024-10-17 21:17:13 +0900

    ✨ feat(progress): show when can scroll

--- app/components/header/_component/progress.tsx
+++ app/components/header/_component/progress.tsx
@@ -1,16 +1,36 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Progress = () => {
 	const pathname = usePathname();
-	if (!pathname.includes("/learn")) return null;
+	const [showProgress, setShowProgress] = useState(false);
+
+	useEffect(() => {
+		const footerHeight = document
+			.getElementsByTagName("footer")[0]
+			.getBoundingClientRect().height;
+		document.documentElement.style.setProperty(
+			"--footer-height",
+			`${footerHeight}px`,
+		);
+
+		const canScroll =
+			document.body.scrollHeight >
+			(window.innerHeight || document.documentElement.clientHeight);
+
+		setShowProgress(canScroll && pathname.includes("/learn"));
+	}, [pathname]);
+
 	return (
-		<div
-			id="progress"
-			style={{ animationTimeline: "--page-scroll" }}
-			className="absolute left-0 bottom-0 w-full h-[2px] bg-base-content origin-[0%_50%] animate-[grow-progress_auto_linear]"
-		/>
+		showProgress && (
+			<div
+				id="progress"
+				style={{ animationTimeline: "--page-scroll" }}
+				className="absolute left-0 bottom-0 w-full h-[2px] bg-base-content origin-[0%_50%] animate-[grow-progress_auto_linear]"
+			/>
+		)
 	);
 };
 

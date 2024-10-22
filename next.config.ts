6addbbcec219cb118967726a1bd2024deceb5069
summerscar <shen.yus@163.com>
2024-10-22 19:20:19 +0900

    🐞 fix(build): vercel build error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import withMDX from "@next/mdx";
-// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./app/i18n.ts");
+
+const SVGRConfig = {
+	svgoConfig: {
+		plugins: [
+			{
+				name: "preset-default",
+				params: {
+					overrides: {
+						cleanupIds: false,
+					},
+				},
+			},
+		],
+	},
+};
 
 const nextConfig: NextConfig = {
 	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
@@ -12,11 +27,61 @@ const nextConfig: NextConfig = {
 		turbo: {
 			rules: {
 				"*.svg": {
-					loaders: ["@svgr/webpack"],
+					loaders: [
+						{
+							loader: "@svgr/webpack",
+							options: SVGRConfig,
+						},
+					],
 					as: "*.js",
 				},
 			},
 		},
+	},
+	webpack(config, { isServer }) {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
+
+		// Grab the existing rule that handles SVG imports
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		const fileLoaderRule = config.module.rules.find((rule: any) =>
+			rule.test?.test?.(".svg"),
+		);
+
+		config.module.rules.push(
+			// Reapply the existing rule, but only for svg imports ending in ?url
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			// Convert all other *.svg imports to React components
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+				use: [
+					{
+						loader: "@svgr/webpack",
+						options: SVGRConfig,
+					},
+				],
+			},
+		);
+
+		// Modify the file loader rule to ignore *.svg, since we have it handled now.
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		config.module.rules.push({
+			test: /\.(mp3)$/,
+			type: "asset/resource",
+			generator: {
+				filename: "static/[name].[hash].[ext]",
+			},
+		});
+
+		return config;
 	},
 };
 

62da8303e18bb8c5dc2a334e6b3792a8055ca9b9
summerscar <shen.yus@163.com>
2024-10-23 17:25:27 +0900

    ✨ feat(docs): loading effect and default cache page

--- app/(home)/learn/[...doc_path]/loading.jsx
+++ app/(home)/learn/[...doc_path]/loading.jsx
@@ -1,1 +1,12 @@
-export { default } from "@/(home)/loading.tsx";
+export default function Loading() {
+	return (
+		<div className="flex w-full flex-col gap-6">
+			<div className="skeleton h-12 w-40" />
+			<div className="skeleton h-8 w-1/3" />
+			<div className="skeleton h-4 w-1/2" />
+			<div className="skeleton h-4 w-3/4" />
+			<div className="skeleton h-4 w-2/3" />
+			<div className="skeleton h-4 w-1/2" />
+		</div>
+	);
+}

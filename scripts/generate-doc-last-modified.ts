842d644777c3fe797cb90432ce36d9939298f136
summerscar <shen.yus@163.com>
2024-10-27 17:44:35 +0900

    ✨ feat(lint-staged): add script to lint-staged

--- scripts/generate-doc-last-modified.ts
+++ scripts/generate-doc-last-modified.ts
@@ -3,16 +3,19 @@ import { readFileSync, writeFileSync } from "node:fs";
 import { flattenAllDocs, insertOrUpdateFrontmatterKey } from "./list-all-docs";
 
 (async () => {
+	const stagedDocs = process.argv.slice(2);
+	console.log("[generate-doc-last-modified]: start...");
+	console.log("[generate-doc-last-modified][staged docs]: ", stagedDocs);
 	const docs = await flattenAllDocs();
-	const docsWithLastModification = docs.map((doc) => {
-		const timeStamp = execSync(
-			`git log -1 --pretty="format:%at" "${doc.path}"`,
-		).toString("utf-8");
-		return {
-			...doc,
-			lastModified: timeStamp,
-		};
-	});
+	const docsWithLastModification = docs
+		.filter((doc) => stagedDocs.includes(doc.path))
+		.map((doc) => {
+			const timeStamp = String(new Date().getTime());
+			return {
+				...doc,
+				lastModified: timeStamp,
+			};
+		});
 	docsWithLastModification.forEach((doc) => {
 		const docString = readFileSync(doc.path, "utf-8");
 		const frontmatterKey = "last-modified";
@@ -24,9 +27,9 @@ import { flattenAllDocs, insertOrUpdateFrontmatterKey } from "./list-all-docs";
 		writeFileSync(doc.path, newDocString);
 		console.log(
 			`[generate-doc-last-modified]: ${doc.title} -> ${frontmatterKey}: ${doc.lastModified} ${new Date(
-				Number(doc.lastModified) * 1000,
+				Number(doc.lastModified),
 			).toLocaleString()}`,
 		);
 	});
-	// console.log("[generate-doc-last-modification]: \n", docsWithLastModification);
+	console.log("[generate-doc-last-modified]: finish...");
 })();

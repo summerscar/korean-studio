c83883c0eba00aab808fa9c2fced2aef95ddf579
summerscar <shen.yus@163.com>
2024-11-08 15:10:03 +0900

    ✨ feat(search): optimize search result

--- scripts/generate-search-index.ts
+++ scripts/generate-search-index.ts
@@ -1,6 +1,9 @@
 import { readFileSync, writeFileSync } from "node:fs";
 import { join, relative } from "node:path";
 import { cwd } from "node:process";
+import { remark } from "remark";
+import remarkGfm from "remark-gfm";
+import strip from "strip-markdown";
 import { flattenAllDocs } from "./list-all-docs";
 
 // 遍历文档目录，提取内容生成索引
@@ -11,10 +14,16 @@ import { flattenAllDocs } from "./list-all-docs";
 		"---------- start -----------\n[generate-search-index]: Generating search index...",
 	);
 	for (const file of files) {
-		const content = await readFileSync(file.path, "utf-8");
-		const resolvedContent = content
-			.replace(/^---([\s\S]*?)---/g, "")
-			.replace(/^[\n]?/g, " ");
+		const { value: content } = await remark()
+			.use(remarkGfm)
+			.use(strip)
+			.process(
+				(await readFileSync(file.path, "utf-8")).replace(
+					/^---([\s\S]*?)---/g,
+					"",
+				),
+			);
+
 		// 将数据添加到 searchData 数组，用于保存到 JSON 文件
 		searchData.push({
 			id: file.title,
@@ -22,7 +31,7 @@ import { flattenAllDocs } from "./list-all-docs";
 			title: file.title,
 			level: file.level,
 			relativeReadablePath: file.relativeReadablePath,
-			content: resolvedContent,
+			content,
 		});
 	}
 

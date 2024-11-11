045907e52737463e8b846eb57a894570320f66dd
summerscar <shen.yus@163.com>
2024-11-11 11:50:06 +0900

    🌈 style(mobile): update style

--- app/(home)/account/layout.tsx
+++ app/(home)/account/layout.tsx
@@ -1,4 +1,6 @@
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div className="flex w-full sm:w-2/3 mx-auto p-8">{children}</div>;
+	return (
+		<div className="flex w-full sm:w-2/3 mx-auto p-4 sm:p-8">{children}</div>
+	);
 };
 export default AccountLayout;

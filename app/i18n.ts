20e5a29d9a28094e4d5323208570888cbcdf6767
summerscar <shen.yus@163.com>
2024-11-14 23:48:57 +0900

    ✨ feat(intl): get language from header

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,12 +1,37 @@
 import { getRequestConfig } from "next-intl/server";
+import { headers } from "next/headers";
 import { getI18nFromCookie } from "./actions/check-i18n";
+import { SITES_LANGUAGE } from "./types/site";
 import { DEFAULT_SITE_LANGUAGE } from "./utils/config";
 
 export default getRequestConfig(async () => {
-	const locale = (await getI18nFromCookie()) || DEFAULT_SITE_LANGUAGE;
+	const locale =
+		(await getI18nFromCookie()) ||
+		(await getLanguageFromHeader()) ||
+		DEFAULT_SITE_LANGUAGE;
 
 	return {
 		locale,
 		messages: (await import(`../messages/${locale}.json`)).default,
 	};
 });
+
+const getLanguageFromHeader = async () => {
+	const header = await headers();
+	const acceptLanguage = header.get("accept-language") || "";
+	const languages = acceptLanguage
+		.split(",")
+		.map((lang) => {
+			const [code, qValue] = lang.split(";q=");
+			return { code: code.trim(), priority: Number.parseFloat(qValue) || 1.0 };
+		})
+		.sort((a, b) => b.priority - a.priority);
+
+	// 查找第一个受支持的语言
+	for (const lang of languages) {
+		if (Object.values(SITES_LANGUAGE).includes(lang.code as SITES_LANGUAGE)) {
+			return lang.code;
+		}
+	}
+	return;
+};

1ab248904e71835540c05ddf9c00f875bcc94392
summerscar <shen.yus@163.com>
2024-11-21 11:48:17 +0900

    ✨ feat(error): update error page

--- app/(home)/error.tsx
+++ app/(home)/error.tsx
@@ -2,19 +2,25 @@
 
 import { useRouter } from "next/navigation";
 
-export default function ErrorPage() {
+export default function ErrorPage({
+	error,
+	// reset,
+}: {
+	error: Error & { digest?: string };
+	reset: () => void;
+}) {
 	const router = useRouter();
 	return (
 		<div className="flex flex-col items-center justify-center p-4 mx-auto">
 			<div className="text-center space-y-6">
-				<h1 className="text-4xl font-bold text-gray-900">出错了</h1>
-				<p className="text-gray-600">抱歉，页面加载出现问题</p>
+				<h1 className="text-4xl font-bold text-gray-900">⚠️ Error</h1>
+				<p className="text-sm text-base-content/60">{error?.message}</p>
 				<button
 					type="button"
 					onClick={() => router.replace("/")}
 					className="px-6 py-2 btn"
 				>
-					返回首页
+					To Home
 				</button>
 			</div>
 		</div>

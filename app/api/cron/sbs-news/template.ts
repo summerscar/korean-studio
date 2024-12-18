9cc8424419b3864a9c881032c20eb8047d95ea9f
summerscar <shen.yus@163.com>
2024-12-18 14:20:30 +0900

    🐞 fix(sbs-news): filter short video

--- app/api/cron/sbs-news/template.ts
+++ app/api/cron/sbs-news/template.ts
@@ -10,12 +10,12 @@ type Video = {
 const buildContent = (video: Video) => {
 	return `
 <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/${video.videoId}"
-    title="${video.title?.replace(/"/g, '\\"')}"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	width="560"
+	height="315"
+	src="https://www.youtube.com/embed/${video.videoId}"
+	title="${video.title?.replace(/"/g, "&quot;")}"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 />
 
 ---
